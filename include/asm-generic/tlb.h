162bcaef74d6c0a4607cbc10de3ed3c0005b9f44
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-02-24 14:42:46 +0600

    treewide: Make macros no-op using `((void)0)`
    Do not solely rely on compiler optimizations to get the workaround
    of having macros do nothing using an empty do-while loop. It's
    inefficient.
    
    Use ((void)0) to which the standard assert macro expands when NDEBUG
    is defined.
    
    No functional change intended.
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Change-Id: I4282dab288c9d774e94c1cb8b80195396ae6cfad
    [TogoFire]: adapt to k5.4.
    Signed-off-by: TogoFire <togofire@mailfence.com>

--- include/asm-generic/tlb.h
+++ include/asm-generic/tlb.h
@@ -583,7 +583,7 @@ static inline void tlb_flush_p4d_range(struct mmu_gather *tlb,
  * This is a nop so far, because only x86 needs it.
  */
 #ifndef __tlb_remove_pmd_tlb_entry
-#define __tlb_remove_pmd_tlb_entry(tlb, pmdp, address) do {} while (0)
+#define __tlb_remove_pmd_tlb_entry(tlb, pmdp, address) ((void)0)
 #endif
 
 #define tlb_remove_pmd_tlb_entry(tlb, pmdp, address)			\
@@ -597,7 +597,7 @@ static inline void tlb_flush_p4d_range(struct mmu_gather *tlb,
  * invalidation. This is a nop so far, because only x86 needs it.
  */
 #ifndef __tlb_remove_pud_tlb_entry
-#define __tlb_remove_pud_tlb_entry(tlb, pudp, address) do {} while (0)
+#define __tlb_remove_pud_tlb_entry(tlb, pudp, address) ((void)0)
 #endif
 
 #define tlb_remove_pud_tlb_entry(tlb, pudp, address)			\
