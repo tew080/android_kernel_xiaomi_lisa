0fb01d672ca66e32666b459587d0fb3253baeeae
tew404 <thammarat.tew@gmail.com>
2024-09-06 06:48:21 +0700

    Update

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8138,11 +8138,11 @@ static void uclamp_set(struct cgroup_subsys_state *css)
 	int i;
 
 	static struct uclamp_param tgts[] = {
-		{"top-app",            "10",  "max",  1, 1, 20480},
+		{"top-app",            "30",  "max",  1, 1, 20480},
 		{"rt",			"0",  "max",  1, 0, 20480},
 		{"nnapi-hal",		"0",  "max",  1, 0, 20480},
-       		{"foreground",          "0",  "max",  1, 0, 20480},
-                {"camera-daemon",      "10",  "max",  1, 1, 20480},
+       		{"foreground",          "10",  "max",  1, 0, 20480},
+                {"camera-daemon",      "30",  "max",  1, 1, 20480},
                 {"system",              "0",  "max",  0, 0, 20480},
                 {"dex2oat",             "0",  "60",   0, 0,   512},
         	{"background",          "0",  "50",   0, 0,  1024},
