3482fffbc66cfaa7f2cd1c0be46ee2dd3e8e3737
tew404 <thammarat.tew@gmail.com>
2024-09-08 12:15:19 +0700

    sched/fair: Modify capacity margins for sm8250

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -133,9 +133,11 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
 
 /* Migration margins */
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
+    1280, 1280, 1280, 1280,1625, 1625, 1625,1920                   
+};
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
+    1024, 1024, 1024, 1024,1796, 1796, 1796,1536                   
+};
 
 #ifdef CONFIG_SCHED_WALT
 __read_mostly unsigned int sysctl_sched_prefer_spread;
