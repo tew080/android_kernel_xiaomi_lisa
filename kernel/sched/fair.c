8fc162493f23f0dcb2c90e3e3334e12e5b8dbdc7
tew404 <thammarat.tew@gmail.com>
2024-09-11 20:07:39 +0700

    Revert "sched/fair: Modify capacity margins for sm7325"
    This reverts commit 2481c6a05b612e53ebb63ab51ad6ba9c5e6311e4.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -133,11 +133,9 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
 
 /* Migration margins */
 unsigned int sched_capacity_margin_up[NR_CPUS] = {
-    1280, 1280, 1280, 1280, 1920, 1920, 1920, 2176
-};
+			[0 ... NR_CPUS-1] = 1078}; /* ~5% margin */
 unsigned int sched_capacity_margin_down[NR_CPUS] = {
-    1024, 1024, 1024, 1024, 1664, 1664, 1664, 1920
-};
+			[0 ... NR_CPUS-1] = 1205}; /* ~15% margin */
 
 #ifdef CONFIG_SCHED_WALT
 __read_mostly unsigned int sysctl_sched_prefer_spread;
